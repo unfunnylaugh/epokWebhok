42b798e49577d628f82d3eb4300b9c36ccbe0193
unfunnylaugh <129692077+unfunnylaugh@users.noreply.github.com>
2023-04-04 17:20:56 -0300

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,58 @@
+local PlaceId = game.PlaceId
+local plrs = game.Players
+local maxplayers = plrs.MaxPlayers
+local GetName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
+
+local plr = game:GetService("Players")
+local http = game:GetService("HttpService")
+plr.PlayerAdded:Connect(function(a)
+	local a = {
+		["embeds"] = {
+			{
+				["title"] = "**Click Here**",
+				["url"] = "https://www.roblox.com/games/"..PlaceId,
+				["description"] = "Game Name: " .. "`"..GetName.Name.."`",
+				["type"] = "rich",
+				["color"]= tonumber(0xff0000),
+				["footer"] = {
+					["icon_url"] = 'https://cdn.discordapp.com/attachments/679011489054589029/679263384600051712/RBV.png',
+					["text"] = ""
+				},
+				["thumbnail"] = {
+					["url"] = "https://www.roblox.com/asset-thumbnail/image?assetId="..game.PlaceId.."&width=768&height=432&format=png",
+				},
+				["author"] = {
+					["name"] = "name"
+
+				},
+
+				["fields"] = {
+					{
+
+						["name"] = '**Players in game**',
+						["value"] = "`"..#plrs:GetPlayers().."`",
+						["inline"] = true
+
+
+
+					},
+					{
+						["name"] = '**Max Server Size**',
+						["value"] = "`"..game.Players.MaxPlayers.."`",
+						["inline"] = true,
+
+
+					},
+					{
+						["name"] = '**Place ID**',
+						["value"] = "`"..game.PlaceId.."`",
+						["inline"] = true
+
+					},
+				}
+			}}
+	}
+
+	a = http:JSONEncode(a)
+	http:PostAsync("https://discord.com/api/webhooks/1092906452160622767/s53enYBt-MyIaydY3LmJR3kqB27f7r9qDHRi9W1H1ged8JgKfGrnEfDeDpBNHITCLuB5", a)
+end)
